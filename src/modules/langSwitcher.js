2b8fb6731d4e0790ce9099b8d8e2c8a863050835
HenriqueAPMendes <hapmendesss@gmail.com>
2023-03-06 12:59:38 -0300

    traducao finalizada

--- src/modules/langSwitcher.js
+++ src/modules/langSwitcher.js
@@ -6,8 +6,13 @@ const data = {
         contact: "Contato",
         nav: '<li><a href="#home">Início</a></li><li><a href="#about-me">Sobre mim<a></li><li><a href="#projects">Projetos</a></li><li><a href="#contact">Contato</a></li> ',
         home: "<p>Olá, sou o Henrique.</p> <p> Sou desenvolvedor e atualmente trabalho com as seguintes tecnologias: </p>",
+        others: "Outros",
         about: "<p>Cursei meu ensino Fundamental e Médio no Colégio da Polícia Militar do Paraná (CPMPR), em Curitiba, Brasil. </p><p>Atualmente cursando Engenharia de Computação na Universidade Tecnológica Federal do Paraná (UTFPR).</p><p> Sempre fui apaixonado por tecnologia e aprender a construir coisas.</p>",
-        curriculum: "Baixe meu currículo em PDF",
+        cards: ["Jogo em C++",
+            "Em breve",
+            "Em breve",
+            "Em breve"
+        ],
         submit: "Enviar",
         placeholders: [
             "Seu nome",
@@ -25,8 +30,13 @@ const data = {
         nav: '<li><a href="#home">Home</a></li><li><a href="#about-me">About me<a></li><li><a href="#projects">Projects</a></li><li><a href="#contact">Contact</a></li>',
 
         home: "<p>Hello, I'm Henrique.</p> <p>I'm a software developer currently working with these technologies: </p>",
+        others: "Others",
         about: " <p>I attended elementary and high school at Colégio da Polícia Militar do Paraná (CPMPR), in Curitiba, Brazil. </p> <p> Nowadays, I'm studying Computer Engineering at Universidade Tecnológica Federal do Paraná (UTFPR). </p><p> I've always been in love with technology and learning to build things. </p>",
-        curriculum: "Download my CV",
+        cards: ["Game in C++",
+            "Coming soon",
+            "Coming soon",
+            "Coming soon"
+        ],
         submit: "Submit",
         placeholders: [
             "Your name",
@@ -41,7 +51,6 @@ const data = {
 const switcher = document.querySelector('.switcher');
 const homeText = document.querySelector('.home-text');
 const aboutMeText = document.querySelector('.about-me-text');
-const cvText = document.querySelector('.download-cv a');
 const nav = document.querySelector('nav ul');
 const readMoreBtns = document.querySelectorAll('.saiba-mais');
 const sourceCodeBtns = document.querySelectorAll('.card button a');
@@ -50,6 +59,8 @@ const projectsTitle = document.querySelector('#projects h2');
 const contactTitle = document.querySelector('#contact h2');
 const placeholders = document.querySelectorAll('.input-box .input ~ span');
 const submitBtn = document.querySelector('form button');
+const cardTitles = document.querySelectorAll('.card h3');
+const others = document.querySelector('.others-stack h3');
 
 export function listenSwitch() {
     switcher.addEventListener('click', () => {
@@ -62,7 +73,6 @@ export function listenSwitch() {
         projectsTitle.innerText = data[lang].projects.toUpperCase();
         contactTitle.innerText = data[lang].contact.toUpperCase();
 
-        cvText.innerHTML = data[lang].curriculum;
         nav.innerHTML = data[lang].nav;
         readMoreBtns.forEach(el => {
             el.innerHTML = data[lang].readMore;
@@ -74,5 +84,9 @@ export function listenSwitch() {
             el.innerHTML = data[lang].placeholders[i];
         });
         submitBtn.innerHTML = data[lang].submit;
+        cardTitles.forEach((el, i) => {
+            el.innerHTML = data[lang].cards[i];
+        });
+        others.innerHTML = data[lang].others;
     });
 }
